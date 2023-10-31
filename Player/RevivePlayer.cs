2da946c68bce1ea8e0c8b8ae77b9b0f96b6ee29b
doomimic <doomimic@gmail.com>
2023-10-31 23:51:19 +0800

    No respawn when boss active

--- Player/RevivePlayer.cs
+++ Player/RevivePlayer.cs
@@ -1,8 +1,22 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ModLoader;
 
 namespace Revive.System
 {
     public class RevivePlayer : ModPlayer
     {
+        public int deadTimer; // Fix for visual issue on respawn timer
+
+        public override void OnEnterWorld() => deadTimer = 0;
+
+        public override void OnRespawn() => deadTimer = 0;
+
+        public override void UpdateDead()
+        {
+            if (Main.CurrentFrameFlags.AnyActiveBossNPC && deadTimer > 0)
+                Player.respawnTimer++;
+
+            deadTimer++;
+        }
     }
 }
