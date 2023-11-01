b90fa17e5f82bf3deb7feb41d1fc67a81aa04ef8
doomimic <doomimic@gmail.com>
2023-11-01 20:39:21 +0800

    Tab to space

--- Players/RevivePlayer.cs
+++ Players/RevivePlayer.cs
@@ -4,22 +4,22 @@ using Terraria.ModLoader;
 
 namespace Revive.Players
 {
-    public class RevivePlayer : ModPlayer
-    {
-        public int deadTimer; // Fix for visual issue on respawn timer
+	public class RevivePlayer : ModPlayer
+	{
+		public int deadTimer; // Fix for visual issue on respawn timer
 
-        public override void OnEnterWorld() => deadTimer = 0;
+		public override void OnEnterWorld() => deadTimer = 0;
 
-        public override void OnRespawn() => deadTimer = 0;
+		public override void OnRespawn() => deadTimer = 0;
 
-        private bool ActiveBossAndAlivePlayer() => Main.CurrentFrameFlags.AnyActiveBossNPC && ModContent.GetInstance<ReviveSystem>().anyAlivePlayer && deadTimer > 0;
+		private bool ActiveBossAndAlivePlayer() => Main.CurrentFrameFlags.AnyActiveBossNPC && ModContent.GetInstance<ReviveSystem>().anyAlivePlayer && deadTimer > 0;
 
-        public override void UpdateDead()
-        {
-            if (ActiveBossAndAlivePlayer())
-                Player.respawnTimer++; // Undoes regular respawn timer tickdown
+		public override void UpdateDead()
+		{
+			if (ActiveBossAndAlivePlayer())
+				Player.respawnTimer++; // Undoes regular respawn timer tickdown
 
-            deadTimer++;
-        }
-    }
+			deadTimer++;
+		}
+	}
 }
--- Systems/ReviveSystem.cs
+++ Systems/ReviveSystem.cs
@@ -4,43 +4,43 @@ using Terraria.ModLoader;
 
 namespace Revive.Systems
 {
-    public class ReviveSystem : ModSystem
-    {
-        public bool oldAnyAlivePlayer;
-        public bool anyAlivePlayer;
+	public class ReviveSystem : ModSystem
+	{
+		public bool oldAnyAlivePlayer;
+		public bool anyAlivePlayer;
 
-        public override void OnWorldLoad()
-        {
-            oldAnyAlivePlayer = true;
-            anyAlivePlayer = true;
-        }
+		public override void OnWorldLoad()
+		{
+			oldAnyAlivePlayer = true;
+			anyAlivePlayer = true;
+		}
 
-        private void UpdateAnyAlivePlayer()
-        {
-            for (int i = 0; i < Main.maxPlayers; i++) {
-                Player player = Main.player[i];
-                if (player.active && !player.dead) {
-                    anyAlivePlayer = true;
-                    return;
-                }
-            }
+		private void UpdateAnyAlivePlayer()
+		{
+			for (int i = 0; i < Main.maxPlayers; i++) {
+				Player player = Main.player[i];
+				if (player.active && !player.dead) {
+					anyAlivePlayer = true;
+					return;
+				}
+			}
 
-            anyAlivePlayer = false;
-        }
+			anyAlivePlayer = false;
+		}
 
-        public override void PostUpdateWorld()
-        {
-            if (Main.netMode != NetmodeID.Server)
-                return;
+		public override void PostUpdateWorld()
+		{
+			if (Main.netMode != NetmodeID.Server)
+				return;
 
-            if (anyAlivePlayer != oldAnyAlivePlayer) {
-                ModPacket packet = Mod.GetPacket();
-                packet.Write(anyAlivePlayer);
-                packet.Send();
-            }
+			if (anyAlivePlayer != oldAnyAlivePlayer) {
+				ModPacket packet = Mod.GetPacket();
+				packet.Write(anyAlivePlayer);
+				packet.Send();
+			}
 
-            oldAnyAlivePlayer = anyAlivePlayer;
-            UpdateAnyAlivePlayer();
-        }
-    }
+			oldAnyAlivePlayer = anyAlivePlayer;
+			UpdateAnyAlivePlayer();
+		}
+	}
 }
