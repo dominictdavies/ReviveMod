f36a40ef332e0ccd0facbc208817ba7c81704ca9
doomimic <doomimic@gmail.com>
2023-11-21 16:53:09 +0800

    Add ReviveAura

---
+++ Projectiles/ReviveAura.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Content.Projectiles
+{
+    public class ReviveAura : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 64; // The width of projectile hitbox
+            Projectile.height = 64; // The height of projectile hitbox
+            Projectile.aiStyle = 0; // The ai style of the projectile, please reference the source code of Terraria
+            Projectile.timeLeft = 600; // The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
+            Projectile.ignoreWater = true; // Does the projectile's speed be influenced by water?
+            Projectile.tileCollide = true; // Can the projectile collide with tiles?
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            // Revive player
+        }
+    }
+}
