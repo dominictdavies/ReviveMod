3fbeae32ea916bb37d7e1a1fd849628c0c7952fa
Dominic Davies <dominictdavies@gmail.com>
2025-02-19 01:27:13 +0800

    Add EoW as a manual boss

--- Source/Common/CommonUtils.cs
+++ Source/Common/CommonUtils.cs
@@ -1,13 +1,22 @@
 ﻿using ReviveMod.Source.Common.Systems;
+using System.Collections.Generic;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ReviveMod.Source.Common
 {
     public class CommonUtils
     {
+        private static readonly HashSet<short> manualBosses = [NPCID.EaterofWorldsHead];
+
         public static bool ActiveBossAlivePlayer
-            => Main.CurrentFrameFlags.AnyActiveBossNPC
-            && ModContent.GetInstance<ReviveModSystem>().anyAlivePlayer;
+        {
+            get {
+                // EoW does not use the boss attribute, so they must be considered manually
+                return (Main.CurrentFrameFlags.AnyActiveBossNPC || Main.npc.Any(npc => npc.active && manualBosses.Contains(npc.type))) 
+                    && ModContent.GetInstance<ReviveModSystem>().anyAlivePlayer;
+            }
+        }
     }
 }
